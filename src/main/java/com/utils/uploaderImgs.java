1af7a0a19ec48db04bc7e8fa4b28731794774c82
胡鹏 <750577590@qq.com>
2017-09-14 20:10:53 +0800

    接口项目

--- src/main/java/com/utils/uploaderImgs.java
+++ src/main/java/com/utils/uploaderImgs.java
@@ -10,26 +10,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 import java.util.Map;
 
 @Controller
 public class uploaderImgs {
 
 
-    private int  counter = 0;
     @RequestMapping("uploaderImgs")
     public void  upload(HttpServletRequest request,HttpServletResponse response) {
 
         System.out.println("收到图片!");
         MultipartHttpServletRequest Murequest = (MultipartHttpServletRequest)request;
         Map<String, MultipartFile> files = Murequest.getFileMap();//得到文件map对象
-        String upaloadUrl = "D:\\test\\upload/";//得到当前工程路径拼接上文件名
+        String time =new MyDateTimeUtils().DateTimeToStr(new Date(), "yyyy-MM-dd").replace("\\s*","");;
+        String upaloadUrl = "D:\\JX3JZ\\"+time+"/";
         File dir = new File(upaloadUrl);
         System.out.println(upaloadUrl);
         if(!dir.exists())//目录不存在则创建
             dir.mkdirs();
         for(MultipartFile file :files.values()){
-            counter++;
             String fileName=file.getOriginalFilename();
             File tagetFile = new File(upaloadUrl,fileName);//创建文件对象
             if(!tagetFile.exists()){//文件名不存在 则新建文件，并将文件复制到新建文件中
@@ -40,7 +40,6 @@ public class uploaderImgs {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
             }
         }
         System.out.println("接收完毕");
