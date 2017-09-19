a8932657c25b20a1bc1a8f3692b930cdc3c8c335
胡鹏 <750577590@qq.com>
2017-09-19 22:17:56 +0800

    接口项目

--- src/main/java/com/dao/userManageDao.java
+++ src/main/java/com/dao/userManageDao.java
@@ -21,7 +21,7 @@ public class userManageDao {
     public List<Map<String,Object>> userManageInfo() throws Exception {
         StringBuilder sql = new StringBuilder();
         List<Object> paramList = new ArrayList<Object>();
-        sql.append(" SELECT a.*,b.START_DATE,b.SERVER_COST,b.SERVER_NUM FROM f_sys_mod_1 a LEFT JOIN f_sys_mod_2 b ON a.mod_id = b.MOD_ID WHERE a.ISVALID=1 ORDER BY a.MOD_ID ");
+        sql.append(" SELECT a.* FROM f_sys_mod_1 a  WHERE a.ISVALID=1 ORDER BY a.MOD_ID ");
         System.out.println(sql);
         return this.commondao.query(sql.toString(), paramList);
     }
